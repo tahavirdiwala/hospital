b2d5d23778f8e7300c708a6ca59fe339c473fb14
tahavirdiwala <tahavirdiwala97@gmail.com>
2024-09-30 17:13:08 +0530

    ..

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 const express = require("express");
 const connectDb = require("./db/connect");
-const SymptomsModel = require("./models/symptoms.model");
 const app = express();
 
 start();

3e1c257835d304a6dc7df2b38504d59f4e82db00
tahavirdiwala <tahavirdiwala97@gmail.com>
2024-10-16 12:11:57 +0530

    add document for token middleware

--- middlewares/token.middleware.js
+++ middlewares/token.middleware.js
@@ -1,6 +1,11 @@
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
 
+/**
+ * Sets a jwt response for specified user.
+ * @param {object} user - User fields object.
+ * @param {string} expiry - expiry for jwt token.
+ */
 const createTokenFor = (user, expiry) => {
   return jwt.sign(user, process.env.JWT_SECRET, {
     expiresIn: expiry,

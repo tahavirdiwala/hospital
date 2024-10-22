c98b6306fb677d8c81bc78510bb991384f28e97a
tahavirdiwala <tahavirdiwala97@gmail.com>
2024-10-22 11:54:30 +0530

    change log

--- server.js
+++ server.js
@@ -23,9 +23,7 @@ async function start() {
       console.log("database connected");
     });
     app.listen(SERVER_CONFIG.PORT, () => {
-      console.log(
-        "server is running on " + `http://localhost:${SERVER_CONFIG.PORT}`
-      );
+      console.log("server is running at port " + SERVER_CONFIG.PORT);
     });
   } catch (error) {
     console.log(error);

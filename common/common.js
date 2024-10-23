a2d5790e1bba3aad4165f70fe2170975bdb2fac4
tahavirdiwala <tahavirdiwala97@gmail.com>
2024-10-23 19:04:52 +0530

    fix naming-convention

--- common/common.js
+++ common/common.js
@@ -5,17 +5,17 @@ const { SALT_PASSWORD_CONFIG } = require("../lib/constant");
 
 /**
  * Sends a JSON response with a status code, message, and optional data.
- * @param {response} res - Express response object.
+ * @param {response} response - Express response object.
  * @param {number} statusCode - HTTP status code.
- * @param {string} response - Response message.
+ * @param {string} dispatch - Dispatch Response message.
  * @param {Array|null} [data=null] - Optional response data.
  */
 
-const sendResponse = (res, statusCode, response, data = null) => {
-  if (response["message"]) response = response["message"];
-  res
+const sendResponse = (response, statusCode, dispatch, data = null) => {
+  if (dispatch["message"]) dispatch = dispatch["message"];
+  response
     .status(statusCode)
-    .json({ statusCode, message: response, ...(data && { data }) });
+    .json({ statusCode, message: dispatch, ...(data && { data }) });
 };
 
 const hashField = async (password) => {

b6c5c4c6818ddb2cecf0acae5382ae38bc6abc85
tahavirdiwala <tahavirdiwala97@gmail.com>
2024-10-30 19:12:51 +0530

    pushing optimize routing

--- routes/main.router.js
+++ routes/main.router.js
@@ -1,11 +1,7 @@
-module.exports = [
-  "auth",
-  "user",
-  "symptom",
-  "doctor",
-  "appointment",
-  "communication",
-  "payment",
-  "clinic",
-  "telemedicineSession",
-].map((path) => require(`./${path}.router`));
+const { getFiles } = require("../common/common");
+
+const routerFiles = getFiles(__dirname);
+routerFiles.splice(routerFiles.indexOf("index.js"), 1);
+routerFiles.splice(routerFiles.indexOf("main.router.js"), 1);
+
+module.exports = routerFiles.map((path) => require(`./${path}`));

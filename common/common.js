c39581dde11c150806edeb5e66eb00da39985aa6
tahavirdiwala <tahavirdiwala97@gmail.com>
2024-12-04 18:57:28 +0530

    documenting common-decorators

--- common/common.js
+++ common/common.js
@@ -5,14 +5,23 @@ const bcrypt = require("bcryptjs");
 const { SaltPasswordConfig } = require("../lib/constant");
 
 class CommonDecorators {
+  /**
+   * @param {string} password - Password string for hashing.
+   */
   async hashField(password) {
     return await bcrypt.hash(password, SaltPasswordConfig.Range);
   }
-
+  /**
+   * @param {string} password - current-password string for hashing.
+   * @param {string} hashedPassword - compare with hashed password.
+   */
   async compare(password, hashedPassword) {
     return await bcrypt.compare(password, hashedPassword);
   }
-
+  /**
+   * @param {string} id - id for deleting given password.
+   * @param {mongoose.Model} model - parameter for mongoose model.
+   */
   async handleRemoveDocument(id, model) {
     const entity = Object.keys(model || {}).toLocaleString();
     /**
@@ -32,6 +41,25 @@ class CommonDecorators {
         .catch(reject);
     });
   }
+  /**
+   * Sends a JSON response with a status code, message, and optional data.
+   * @param {response} response - Express response object.
+   * @param {number} statusCode - HTTP status code.
+   * @param {string} dispatch - Dispatch Response message.
+   * @param {Array|null} [data=null] - Optional response data.
+   */
+  sendResponse(response, statusCode, dispatch, data = null) {
+    if (dispatch["message"]) dispatch = dispatch["message"];
+    response
+      .status(statusCode)
+      .json({ statusCode, message: dispatch, ...(data && { data }) });
+  }
+  /**
+   * @param {string} dir - current directory for files.
+   */
+  getFiles(dir) {
+    return fs.readdirSync(dir);
+  }
 
   validator(fields, body) {
     let message = "";
@@ -47,23 +75,6 @@ class CommonDecorators {
     if (message.length > 0) {
       throw new Error(message);
     }
-  }
-  /**
-   * Sends a JSON response with a status code, message, and optional data.
-   * @param {response} response - Express response object.
-   * @param {number} statusCode - HTTP status code.
-   * @param {string} dispatch - Dispatch Response message.
-   * @param {Array|null} [data=null] - Optional response data.
-   */
-  sendResponse(response, statusCode, dispatch, data = null) {
-    if (dispatch["message"]) dispatch = dispatch["message"];
-    response
-      .status(statusCode)
-      .json({ statusCode, message: dispatch, ...(data && { data }) });
-  }
-
-  getFiles(dir) {
-    return fs.readdirSync(dir);
   }
 }
 

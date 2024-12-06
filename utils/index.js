9cbc4719cc5bfd933fa7e02dc386689c7405fafd
tahavirdiwala <tahavirdiwala97@gmail.com>
2024-12-06 11:33:12 +0530

    provide docs for static decorators

--- utils/index.js
+++ utils/index.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose");
 const commonStatic = {
   decorators: {
-    /** @this {mongoose.Model} */
+    /**
+     * @param {import("express").Request} payload has one argument name payload for every get api request
+     * @returns {mongoose.Model} mongoose find query promise with predefined pagination and population by default
+     */
     async findAll(payload = {}) {
       const { page = 1, limit = 10, populate = [], ...rest } = payload;
       return new Promise((resolve, reject) => {
